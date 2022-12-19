e66fbbab9f8e78dec7214dd9054e2aa862bce258
Minsoo <markkim1@naver.com>
2022-12-19 14:26:45 +0900

    save data in a.txt added

--- Naver/find_stock_price.py
+++ Naver/find_stock_price.py
@@ -1,9 +1,13 @@
 import requests
 from bs4 import BeautifulSoup
 
-data = requests.get('https://finance.naver.com/item/sise.naver?code=005930')
 
-def print_price(data):
+categories = ['005930', '066575', '005380', '035720', '034220', '003490']
+
+def print_price(code):
+    # src = 'https://finance.naver.com/item/sise.naver?code={}'.format(code)
+    src = f'https://finance.naver.com/item/sise.naver?code={code}'
+    data = requests.get(src)
     soup = BeautifulSoup(data.content, 'html.parser')
     companyName = soup.select('.wrap_company a')[0].text
     currentPrice = soup.select('#_nowVal')[0].text
@@ -11,5 +15,13 @@ def print_price(data):
     print('name of company is ' + companyName)
     print('current price is ' + currentPrice)
     print('transaction quantity is ' + transactionQuantity)
+    return companyName + ": " + currentPrice + " / " + transactionQuantity
 
-print_price(data)
+# print_price('005930')
+# print_price('066575')
+
+f = open('a.txt', 'w')
+for i in range(len(categories)):
+    f.write(print_price(categories[i]) + "\n")
+
+f.close()
